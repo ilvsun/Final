6886136c50f289cb52a1988462605513248e7da1
trang2609 <150500561+trang2609@users.noreply.github.com>
2023-12-29 13:07:50 +0700

    Add files via upload

--- indext_script.js
+++ indext_script.js
@@ -80,3 +80,30 @@ var giohang = new Array();
         }
 
 
+
+
+
+        function opentab(evt, cityName) {
+            // Declare all variables
+            var i, tabcontent, tablinks;
+          
+            // Get all elements with class="tabcontent" and hide them
+            tabcontent = document.getElementsByClassName("tabcontent");
+            for (i = 0; i < tabcontent.length; i++) {
+              tabcontent[i].style.display = "none";
+            }
+          
+            // Get all elements with class="tablinks" and remove the class "active"
+            tablinks = document.getElementsByClassName("tablinks");
+            for (i = 0; i < tablinks.length; i++) {
+              tablinks[i].className = tablinks[i].className.replace(" active", "");
+            }
+          
+            // Show the current tab, and add an "active" class to the button that opened the tab
+            document.getElementById(cityName).style.display = "block";
+            evt.currentTarget.className += " active";
+          }
+
+          // Mô phỏng một sự kiện click trên nút đầu tiên để làm cho tab đầu tiên được chọn mặc định
+            document.querySelector('.tablinks').click();
+
