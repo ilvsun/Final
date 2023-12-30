79e9637be40bd7e0cff3a4f7afdb581624a55a41
trang2609 <150500561+trang2609@users.noreply.github.com>
2023-12-30 07:19:46 +0700

    Add files via upload

--- sanphamyeuthich.js
+++ sanphamyeuthich.js
@@ -1,18 +1,9 @@
-function showgiohang() {
-    var gh = sessionStorage.getItem("giohang");
-    var giohang = JSON.parse(gh);
-    var ttgh = "";
+$(document).on('click','.xoa',function(e){
+    e.preventDefault();
+    var myTr = $(this).closest('tr');
+    myTr.remove();
+});
 
-    for (let i = 0; i < giohang.length; i++) {
-        ttgh +=
-            '<tr>' +
-            '<td><img src="' + giohang[i][0] + '" alt=""></td>' +
-            '<td>' + giohang[i][1] + '</td>' +
-            '<td class="dongia">' + giohang[i][2] + '</td>' +
-            '<td class="xoasp"><i class="far fa-times-circle"></i></td>'+
-            '<td class="themvaogio"><button class="add-to-cart-button">Thêm vào giỏ hàng</button></td>' +
-            '</tr>';
-    }
-
-    document.getElementById("mycart").innerHTML = ttgh;
+function themvaogio(x) {
+    alert("Đã thêm vào giỏ hàng thành công!");
 }
