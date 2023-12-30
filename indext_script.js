cb93c9a6eefc7d5fbe24e2dc76c7cd26d10e94b4
trang2609 <150500561+trang2609@users.noreply.github.com>
2023-12-30 11:21:56 +0700

    Update indext_script.js

--- indext_script.js
+++ indext_script.js
@@ -154,6 +154,50 @@ function closePopup() {
 }
 /*----------------Popup-----------------------*/
 
+/*----------------Live Chat-----------------------*/
+
+const popup = document.querySelector('.chat-popup');
+const chatBtn = document.querySelector('.chat-btn');
+const submitBtn = document.querySelector('.submit');
+const chatArea = document.querySelector('.chat-area');
+const inputElm = document.querySelector('input');
+const emojiBtn = document.querySelector('#emoji-btn');
+const picker = new EmojiButton();
+
+// Chọn emoji  
+window.addEventListener('DOMContentLoaded', () => {
+
+    picker.on('emoji', emoji => {
+      document.querySelector('input').value += emoji;
+    });
+  
+    emojiBtn.addEventListener('click', () => {
+      picker.togglePicker(emojiBtn);
+    });
+  });        
+
+// bật tắt live chat 
+
+chatBtn.addEventListener('click', ()=>{
+    popup.classList.toggle('show');
+})
+
+// send msg 
+submitBtn.addEventListener('click', ()=>{
+    let userInput = inputElm.value;
+
+    let temp = `<div class="out-msg">
+    <span class="my-msg">${userInput}</span>
+    <img src="images/logo/kiyeu.jpg" class="avatar">
+    </div>`;
+
+    chatArea.insertAdjacentHTML("beforeend", temp);
+    inputElm.value = '';
+
+})
+
+/*----------------Live Chat-----------------------*/
+
 
 /*----------------Nút quay lại đầu trang--------------*/
 $(document).ready(function(){
