2a4db9b423c46ac64a7ba035265de2fae6fcbbd3
trang2609 <150500561+trang2609@users.noreply.github.com>
2023-12-30 07:17:54 +0700

    Add files via upload

--- indext_script.js
+++ indext_script.js
@@ -1,5 +1,4 @@
-
-
+/*----------------Carousel-------------*/
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
@@ -18,105 +17,143 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+/*----------------Carousel-------------*/
 
-//pro
-window.onresize = function(event) {
-    reloadSlider();
-};
+/*----------------Banner Swiper-------------*/
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+/*----------------Banner Swiper-------------*/
 
-//Banner Swiper
-
-    var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 1,
-      spaceBetween: 30,
-      loop: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
-
-
+/*----------------Thêm SP vào giỏ hàng--------------*/
 //khi ấn vào nút giỏ hàng thì tăng thêm đơn vị
 var giohang = new Array();
 
-        function themvaogiohang(x) {
-            var pro = x.parentElement.children;
-            var hinh = pro[0].src;
-            var tensp = pro[1].children[1].innerText;
-            var gia = x.parentElement.querySelector('h4 span').innerText; //lấy ra chuỗi 250.000
-            var giasp = gia.replace(/[^\d]/g, ''); // lấy ra số 250000
-           // var soluong = 1;
-            var sp = new Array(hinh, tensp, giasp); //soluong);
+function themvaogiohang(x) {
+  alert("Đã thêm vào giỏ hàng thành công!");
+  var pro = x.parentElement.children;
+  var hinh = pro[0].children[0].src;
+  var tensp = pro[1].children[0].innerText;
+  var gia = x.parentElement.querySelector('h4 span').innerText; //lấy ra chuỗi 250.000
+  var giasp = gia.replace(/[^\d]/g, ''); // lấy ra số 250000
+  var sp = new Array(hinh, tensp, giasp); //soluong);
 
-            //ktra trong giỏ hàng
-        var kt=0;
-        for (let i=0;i<giohang.length;i++){
-        if(giohang[i][1]==tensp){
-        kt=1;
-        break;
-        }
-        }
-        if(kt==0){
-            //thêm mới
-            giohang.push(sp);
-        }
-
-
-           // console.log(giohang);
-            showcountsp();
-
-            //lưu giỏ hàng lên sesionStorage 
-
-            sessionStorage.setItem("giohang",JSON.stringify(giohang));
-        }
-
-        function showcountsp() {
-            document.getElementById("countsp").innerHTML = giohang.length;
-        }
-function opentab(evt, cityName) {
-            // Declare all variables
-            var i, tabcontent, tablinks;
-          
-            // Get all elements with class="tabcontent" and hide them
-            tabcontent = document.getElementsByClassName("tabcontent");
-            for (i = 0; i < tabcontent.length; i++) {
-              tabcontent[i].style.display = "none";
-            }
-          
-            // Get all elements with class="tablinks" and remove the class "active"
-            tablinks = document.getElementsByClassName("tablinks");
-            for (i = 0; i < tablinks.length; i++) {
-              tablinks[i].className = tablinks[i].className.replace(" active", "");
-            }
-          
-            // Show the current tab, and add an "active" class to the button that opened the tab
-            document.getElementById(cityName).style.display = "block";
-            evt.currentTarget.className += " active";
-          }
-
-          // Mô phỏng một sự kiện click trên nút đầu tiên để làm cho tab đầu tiên được chọn mặc định
-            document.querySelector('.tablinks').click();
-
-
-
-/*----------------Thông báo khi khách hàng nhập email-----------------------*/
-function showAlert() {
-  // Lấy giá trị của input
-  var email = document.getElementById("emailinput").value;
-
-  // Kiểm tra xem ô nhập liệu có rỗng hay không
-  if (email.trim() === "") {
-    alert("Vui lòng nhập email của bạn!");
-  } else {
-    alert("Email của bạn là: " + email + ". Hãy đợi thông báo từ Nutvigor nhé!");
-  }
+  //ktra trong giỏ hàng
+var kt=0;
+for (let i=0;i<giohang.length;i++){
+if(giohang[i][1]==tensp){
+kt=1;
+break;
 }
+}
+if(kt==0){
+  //thêm mới
+  giohang.push(sp);
+}
+
+
+ // console.log(giohang);
+  showcountsp();
+
+  //lưu giỏ hàng lên sesionStorage 
+
+  sessionStorage.setItem("giohang",JSON.stringify(giohang));
+}
+
+function showcountsp() {
+  document.getElementById("countsp").innerHTML = giohang.length;
+}
+
+function themvaogiohangg(x) {
+alert("Đã thêm vào giỏ hàng thành công!");
+}
+
+/*----------------Thêm SP vào giỏ hàng--------------*/
+
+
+
+/*----------------Tabs SP Mới và SP Hot--------------*/
+function opentab(evt, cityName) {
+var i, tabcontent, tablinks;
+          
+tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+document.getElementById(cityName).style.display = "block";
+evt.currentTarget.className += " active";
+}
+
+document.querySelector('.tablinks').click();
+/*----------------Tabs SP Mới và SP Hot--------------*/
+
+
+/*----------------Thông báo thêm SP vào trang DS SP yêu thích--------------*/
+function themvaoyeuthich(x) {
+  alert("Đã thêm vào danh sách yêu thích!");
+}
+/*----------------Thông báo thêm SP vào trang DS SP yêu thích--------------*/
+
+
+
 /*----------------Thông báo khi khách hàng nhập email-----------------------*/
+document.addEventListener("DOMContentLoaded", function() {
+  var emailInput = document.getElementById("emailInput");
+  var subscribeButton = document.querySelector(".normal-nlt");
+
+  subscribeButton.addEventListener("click", function() {
+      var userEmail = emailInput.value;
+
+      if (validateEmail(userEmail)) {
+          alert("Đăng ký thành công! Cảm ơn bạn đã đăng ký thông tin. Chờ tin của Nutvigor nhé!");
+      } else {
+          alert("Địa chỉ email không hợp lệ. Vui lòng kiểm tra lại.");
+      }
+  });
+
+  function validateEmail(email) {
+      // Sử dụng một biểu thức chính quy đơn giản để kiểm tra định dạng email
+      var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return emailRegex.test(email);
+  }
+});
+/*----------------Thông báo khi khách hàng nhập email-----------------------*/
+
+/*----------------Popup-----------------------*/
+
+setTimeout(function() {
+  document.getElementById('popup').style.display = 'block';
+  document.getElementById('overlay').style.display = 'block';
+  
+  // Đóng popup sau 10 giây
+  setTimeout(function() {
+      closePopup();
+  }, 10000);
+}, 1000);
+
+// Hàm đóng popup
+function closePopup() {
+  document.getElementById('popup').style.display = 'none';
+  document.getElementById('overlay').style.display = 'none';
+}
+/*----------------Popup-----------------------*/
+
 
 /*----------------Nút quay lại đầu trang--------------*/
 $(document).ready(function(){
