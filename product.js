7f62a22cbb85a857c6c192bcaf9dfe9ab604869c
trang2609 <150500561+trang2609@users.noreply.github.com>
2023-12-30 12:57:05 +0700

    Add files via upload

--- product.js
+++ product.js
@@ -73,3 +73,44 @@ function applyPriceFilter() {
     });
 */
 
+
+
+/*----------------Thêm SP vào giỏ hàng--------------*/
+//khi ấn vào nút giỏ hàng thì tăng thêm đơn vị
+var giohang = new Array();
+
+function themvaogio(x) {
+  alert("Đã thêm vào giỏ hàng thành công!");
+  var pro = x.parentElement.children;
+  var hinh = pro[0].children[0].src;
+  var tensp = pro[1].children[0].innerText;
+  var gia = x.parentElement.querySelector('h4 span').innerText; //lấy ra chuỗi 250.000
+  var giasp = gia.replace(/[^\d]/g, ''); // lấy ra số 250000
+  var sp = new Array(hinh, tensp, giasp); //soluong);
+
+  //ktra trong giỏ hàng
+var kt=0;
+for (let i=0;i<giohang.length;i++){
+if(giohang[i][1]==tensp){
+kt=1;
+break;
+}
+}
+if(kt==0){
+  //thêm mới
+  giohang.push(sp);
+}
+
+
+ // console.log(giohang);
+  showcountsp();
+
+  //lưu giỏ hàng lên sesionStorage 
+
+  sessionStorage.setItem("giohang",JSON.stringify(giohang));
+}
+
+function showcountsp() {
+  document.getElementById("countsp").innerHTML = giohang.length;
+}
+
