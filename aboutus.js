9d16e9723a7895a18a150d1b30d4303ab05743ff
whyrsf <153831149+whyrsf@users.noreply.github.com>
2023-12-31 21:15:52 +0700

    Update aboutus.js

--- aboutus.js
+++ aboutus.js
@@ -1,10 +1,9 @@
-
 document.addEventListener("DOMContentLoaded", function () {
   const moreContent = document.getElementById("more-content");
   const readMoreBtn = document.getElementById("read-more-btn");
 
   readMoreBtn.addEventListener("click", function (event) {
-      event.preventDefault(); // Ngăn chặn hành vi mặc định của thẻ 'a'
+      event.preventDefault();
 
       if (moreContent.classList.contains("hidden")) {
           moreContent.classList.remove("hidden");
@@ -35,15 +34,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 //Hiệu ứng
-// Đợi tài liệu được tải xong
 document.addEventListener("DOMContentLoaded", function () {
-  // Thêm animation cho các phần tử khi cuộn trang
   window.addEventListener("scroll", reveal);
-
-  // Khai báo hàm reveal
   function reveal() {
     let reveals = document.querySelectorAll(".reveal");
-
     for (let i = 0; i < reveals.length; i++) {
       let windowHeight = window.innerHeight;
       let revealTop = reveals[i].getBoundingClientRect().top;
@@ -55,7 +49,3 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 });
-
-
-
-
